7688d2c2105daf55b99065b3c4e1fa93d6b8135a
ИСП-31 <-31@LAB17-08>
2021-01-26 14:25:28 +0300

    Добавьте файлы проекта.

---
+++ ПР_18/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ПР_18
+{
+    public partial class General_Form : Form
+    {
+        public General_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
